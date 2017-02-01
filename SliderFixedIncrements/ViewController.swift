dc97b5ac8336a71e8c6678a213b5fb077ce50b0f
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2017-02-01 17:36:35 -0500

    Initial Commit

---
+++ SliderFixedIncrements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderFixedIncrements
+//
+//  Created by Lawrence F MacFadyen on 2017-02-01.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
